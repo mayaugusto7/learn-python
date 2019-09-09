1721d8584f9274f661bac4906648265052fa2112
Maycon Ribeiro <maycon.guimaraes@mpgo.mp.br>
2019-09-09 16:36:05 -0300

    Python learn

---
+++ basic/calendar_sample.py
@@ -0,0 +1,5 @@
+import calendar
+
+cal = calendar.month(2019, 9)
+print("Here is the calendar...")
+print(cal)
---
+++ basic/functions.py
@@ -0,0 +1,7 @@
+def printme(str):
+    "This prints a passed string into this function"
+    print(str)
+    return
+
+
+printme("Hello, Maycon Augusto!")
